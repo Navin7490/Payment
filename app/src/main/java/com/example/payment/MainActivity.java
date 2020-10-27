18ef050cd0dd95018b75c194e2a5403e8a75d212
ngama <ngamar018@gmail.com>
2020-10-27 08:41:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/payment/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements PaymentResultListener {
+    Button btnpay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Checkout.preload(getApplicationContext());
+        btnpay=findViewById(R.id.Btn_Pay);
+        btnpay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SetUpPayment();
+            }
+        });
+    }
+
+    private void SetUpPayment() {
+        Checkout checkout = new Checkout();
+
+
+        final Activity activity = this;
+
+        /**
+         * Pass your payment options to the Razorpay Checkout as a JSONObject
+         */
+        try {
+            JSONObject options = new JSONObject();
+
+            options.put("name", "Merchant Name");
+            options.put("description", "Reference No. #123456");
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            //  options.put("order_id", "order_DBJOWzybf0sJbb");//from response of step 3.
+            options.put("theme.color", "#3399cc");
+            options.put("currency", "INR");
+            options.put("amount", "5000");//pass amount in currency subunits
+            options.put("prefill.email", "gaurav.kumar@example.com");
+            options.put("prefill.contact","9988776655");
+            checkout.open(activity, options);
+        } catch(Exception e) {
+            Log.e("TAG", "Error in starting Razorpay Checkout", e);
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(this, "Payment SuccessFull"+s, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(this, "Payment Failed"+s, Toast.LENGTH_SHORT).show();
+
+    }
+}
